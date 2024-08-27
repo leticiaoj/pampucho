a70bc1582d7a5ca3991751a8ef671d2684fb3229
Leticia Oliveira <115517598+leticiaoj@users.noreply.github.com>
2024-08-27 01:05:10 -0300

    video ok

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
-window.addEventListener("resize", function(){
-    const video = document.querySelector(".video1");
-    const section = document.querySelector(".section");
-    const overlay = document.querySelector(".overlay");   
-    
-    section.style.height = video.offsetHeight + "px";
-    overlay.style.height = video.offsetHeight + "px";
+// index.js
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('backgroundVideo');
+    const section = document.querySelector('.section');
+    const overlay = document.querySelector('.overlay');
 
-}, true)
+    function updateVideoSource() {
+        if (window.innerWidth <= 768) {
+            video.src = 'assets/videos/vertical.mp4';
+        } else {
+            video.src = 'assets/videos/horizontal.mp4';
+        }
+    }
+
+    function adjustSectionHeight() {
+        section.style.height = video.offsetHeight + 'px';
+        overlay.style.height = video.offsetHeight + 'px';
+    }
+
+    // Atualize a fonte do vídeo e ajuste a altura ao carregar a página
+    updateVideoSource();
+    adjustSectionHeight();
+
+    // Atualize a fonte do vídeo e ajuste a altura quando a janela é redimensionada
+    window.addEventListener('resize', () => {
+        updateVideoSource();
+        adjustSectionHeight();
+    });
+});
